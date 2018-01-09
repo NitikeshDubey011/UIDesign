a9d4b3d358d03f58fd4e73c901e57a559ff0dfcf
Nitikesh Dubey <nitikeshdubey101@gmail.com>
2018-01-09 14:19:34 +0530

    ProfileUI added

--- app/src/main/java/com/collasping_toolbar/nitikesh/uidesign/MainActivity.java
+++ app/src/main/java/com/collasping_toolbar/nitikesh/uidesign/MainActivity.java
@@ -1,13 +1,26 @@
 package com.collasping_toolbar.nitikesh.uidesign;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.CardView;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+    CardView cardView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        cardView=(CardView)findViewById(R.id.cardViewFull);
+        clicks(cardView);
+
+    }
+
+    public void clicks(View view){
+        Intent intent=new Intent(MainActivity.this,ProfileUI.class);
+        startActivity(intent);
+        finish();
     }
 }
